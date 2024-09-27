6b20b7ba6e73e6164dd942f68fdea631baeb8ef4
Koya Ebata <koya.ebata@frontier-gr.jp>
2024-09-27 22:14:52 +0900

    fix

--- lib/components/label-content/formLabelContent.d.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export type FormLabelContentProps = {
-    label?: string;
-    labelSub?: string;
-    errorText?: string;
-    children?: React.ReactNode | string;
-};
-export declare const FormLabelContent: ({ label, children, errorText, }: FormLabelContentProps) => React.ReactNode;
--- lib/components/label-content/labelContent.d.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-export type LabelContentProps = {
-    label?: string;
-    children?: React.ReactNode | string;
-};
-export declare const LabelContent: ({ label, children, }: LabelContentProps) => React.ReactNode;
